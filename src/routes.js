a234cfbfa84d9b609b6b937717c04d84b3a2ee4b
André Macena <andrmacena_@outlook.com>
2019-12-26 21:41:53 -0300

    Alterado routes.js, adicionado rota para geração do relatorio

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ const express = require('express')
 const UserController = require('./controllers/UserController')
 const AddressController = require('./controllers/AddressController')
 const TechController = require('./controllers/TechController')
+const ReportController = require('./controllers/ReportController')
 
 const routes = express.Router()
 
@@ -15,5 +16,7 @@ routes.get('/users/:user_id/addresses', AddressController.getAddresses)
 routes.post('/users/:user_id/techs', TechController.store)
 routes.get('/users/:user_id/techs', TechController.getTechs)
 routes.delete('/users/:user_id/techs', TechController.delete)
+
+routes.get('/reports', ReportController.show)
 
 module.exports = routes
